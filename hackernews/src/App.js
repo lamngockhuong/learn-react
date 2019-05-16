d90698ef62d78b541ae424a1414a4f424c165094
Lam Ngoc Khuong <me@ngockhuong.com>
2019-05-16 21:51:48 +0700

    ES6 Arrow Functions

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -24,16 +24,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {list.map(function(item) {
-          return (
-            <div key={item.objectId}>
-              <span><a href={item.url}>{item.title}</a></span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          );
-        })}
+        {list.map(item => 
+          <div key={item.objectId}>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        )}
       </div>
     );
   }
