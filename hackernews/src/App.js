10942263982409ac5be5dcef5d35c735dc74a50d
Lam Ngoc Khuong <me@ngockhuong.com>
2019-05-16 23:18:22 +0700

    Local Component State

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -21,10 +21,18 @@ const list = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      list: list,
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        {list.map(item => 
+        {this.state.list.map(item => 
           <div key={item.objectId}>
             <span><a href={item.url}>{item.title}</a></span>
             <span>{item.author}</span>
