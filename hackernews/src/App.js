ee382b2e33373f82dd581afe45d4d5bb48abab46
Lam Ngoc Khuong <me@ngockhuong.com>
2019-05-20 21:43:31 +0700

    ES6 Spread Operators

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -45,10 +45,11 @@ class App extends Component {
   }
 
   onDismiss(id) {
-    // const updatedList = this.state.list.filter(item => item.objectID !== id);
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({
+      result: { ...this.state.result, hits: updatedHits }
+    });
   }
 
   componentDidMount() {
